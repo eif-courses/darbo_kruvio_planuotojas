c8f10f233caefb453d1573535d108e28e70f48d5
Marius <m.gzegozevskis@gmail.com>
2023-11-30 00:18:27 +0200

    qqq

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine('mysql://root:@localhost/kruvio_skirstymas')
+
+
+# Save the DataFrame to a new table in the SQL database
+
 
 def studiju_plano_pravalymas(ekselio_dokumentas):
     dieninis = pd.read_excel(ekselio_dokumentas, sheet_name='Dieninis', header=8)
@@ -6,11 +13,12 @@ def studiju_plano_pravalymas(ekselio_dokumentas):
 
     grupe = pd.read_excel(ekselio_dokumentas, sheet_name='Grupes', header=1)
     merged_data = pd.merge(dieninis_pravalytas, grupe, how='inner', on='Semestras')
-    output_excel_path = 'rezultatas.xlsx'
+    output_excel_path = 'kosmosas.xlsx'
 
     # Write the merged DataFrame to a new Excel file
     merged_data.to_excel(output_excel_path, index=False)  # Set index to False if you don't want to write row numbers
 
+    merged_data.to_sql('my_table', con=engine, index=False, if_exists='replace')
 
     # english = pd.read_excel(ekselio_dokumentas, sheet_name='English',
     #                         header=8)
@@ -30,3 +38,13 @@ def studiju_plano_pravalymas(ekselio_dokumentas):
 
 studiju_plano_pravalymas('planas.xlsx')
 
+
+
+selected_columns = ['Pavadinimas', 'Semestras']
+
+# Read only the specified columns from the SQL database into a DataFrame
+table_name = 'my_table'  # Replace with the name of your table
+query = f"SELECT {', '.join(selected_columns)} FROM {table_name}"
+df = pd.read_sql(query, con=engine)
+# Display the DataFrame
+print(df)
